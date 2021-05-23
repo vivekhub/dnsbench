f89baf26f5e17f4b1a01734dc87ff5901b9976bf
Vivek Venugopalan <github@vivekv.info>
2021-05-23 11:58:57 +0530

    Added formatted output using a loop

--- bench.py
+++ bench.py
@@ -10,14 +10,12 @@ DNS_SERVERS = {
     "Quad9-1": "9.9.9.9",
     "Quad9-2": "149.112.112.112",
     "Softlayer": "169.38.73.5",
-    "Reliance-1": "202.138.120.86",
-    "Reliance-2": "202.138.120.4",
-    "Reliance-3": "202.138.120.6",
+    "Reliance-India-1": "202.138.120.86",
+    "Reliance-India-2": "202.138.120.4",
+    "Reliance-India-3": "202.138.120.6",
     "artech": "202.86.251.201",
     "SignNet": "115.42.228.246",
     "Mobile1-Singapore-1": "118.189.211.221",
-    "DigitalOcean-Singapore-1": "128.199.241.188",
-    "DigitalOcean-Singapore-2": "128.199.102.32",
     "Default Server": None,
 }
 
@@ -25,7 +23,7 @@ DNS_SERVERS = {
 def query_dns(domain):
     responses = []
     for name, server in DNS_SERVERS.items():
-        my_resolver = dns.resolver.Resolver()
+        my_resolver = dns.resolver.Resolver(configure=False)
         if server:
             my_resolver.nameservers = [
                 server,
@@ -33,7 +31,7 @@ def query_dns(domain):
         try:
             resp = my_resolver.resolve(domain, "A")
             timing = resp.response.time * 1000
-            print( f"Processed : {name}")
+            print(f"Processed : {name}")
             responses.append(
                 (
                     name,
@@ -43,9 +41,9 @@ def query_dns(domain):
         except Exception as e:
             print(name, ":failed" + str(e))
     responses = sorted(responses, key=lambda x: x[1])
-    print(f"Best option 1: {responses[0][0]} ({responses[0][1]} msecs)")
-    print(f"Best option 2: {responses[1][0]} ({responses[1][1]} msecs)")
-    print(f"Best option 3: {responses[2][0]} ({responses[2][1]} msecs)")
+
+    for i in range(0, 3):
+        print(f"Best option {i+1}: {responses[i][0]} ({responses[i][1]:.2f} msecs)")
 
 
 query_dns("www.vivekv.info")
